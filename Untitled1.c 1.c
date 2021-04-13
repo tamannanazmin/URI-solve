2af1252410f925836758088448ab253fff4322b6
Tamanna Nazmin <tamanna.naz98@gmail.com>
2021-04-14 05:07:49 +0600

    changed

--- Untitled1.c 1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-
-    {
-       printf("my name is tamanna nazmin , and i am learning c programming which is very disturbing .but i will try my best " );
-       return 0;
-    }
